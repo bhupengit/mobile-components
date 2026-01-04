0760f3cbc6572bc499f9525eb787ada05a8b16cd
bhupengit <bhupenjoshi07@gmail.com>
2026-01-04 20:26:57 +0530

    dropdown initiated

---
+++ app/src/components/dropdown.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function dropdown() {
+  return (
+    <View>
+      <Text>dropdown</Text>
+    </View>
+  )
+}
