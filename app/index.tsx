8448970feac7791dd3fa3e8c2de0befd1371560d
bhupengit <bhupenjoshi07@gmail.com>
2026-01-15 17:23:07 +0530

    Button example

--- app/index.tsx
+++ app/index.tsx
@@ -19,6 +19,12 @@ export default function Index() {
         loading={false}
         fullWidth
       />
+
+      <Button
+        title="Login"
+        leftIcon={<Icon name="log-in" size={18} color="#fff" />}
+        rightIcon={<Icon name="arrow-right" size={18} color="#fff" />}
+      />
     </View>
   );
 }
