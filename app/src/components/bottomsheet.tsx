4b41f988fca6858bd7fe99820c4192070fbda49f
bhupengit <bhupenjoshi07@gmail.com>
2026-01-18 22:27:14 +0530

    Basic Update

--- app/src/components/bottomsheet.tsx
+++ app/src/components/bottomsheet.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default function bottomsheet() {
   return (
     <View>
-      <Text>Bottomsheet</Text>
+      <Text>Bottom Actionsheet</Text>
     </View>
   )
 }
