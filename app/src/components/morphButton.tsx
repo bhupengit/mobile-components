5926d1d8045e73d7b494e23fef77a95787fe8bf6
bhupengit <bhupenjoshi07@gmail.com>
2026-02-25 20:35:12 +0530

    Basic Update

--- app/src/components/morphButton.tsx
+++ app/src/components/morphButton.tsx
@@ -7,7 +7,7 @@ import Animated, {
     withTiming
 } from "react-native-reanimated";
 
-const BUTTON_WIDTH = 260;
+const BUTTON_WIDTH = 300;
 const BUTTON_HEIGHT = 60;
 const RESET_DELAY = 4000; // ms
 
