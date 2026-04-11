a2b2b42494c28753d9f75eca8485b626bcabfed5
bhupengit <bhupenjoshi07@gmail.com>
2026-04-11 21:09:59 +0530

    Basic update

--- app/src/screens/login.tsx
+++ app/src/screens/login.tsx
@@ -4,7 +4,7 @@ import { Button, Text, View } from 'react-native'
 export default function login() {
   return (
     <View>
-      <Text style={{ fontSize: 22, fontWeight: 'bold' }}>Login Screen</Text>
+      <Text style={{ fontSize: 22, fontWeight: 'bold' }}>Authentication Screen</Text>
       <Button title="Login" onPress={() => {}} />
     </View>
   )
