a6d32db55205a18726551fc70df2709fafd0e060
bhupengit <bhupenjoshi07@gmail.com>
2026-04-28 17:39:46 +0530

    basic update

--- app/src/screens/splash.tsx
+++ app/src/screens/splash.tsx
@@ -143,7 +143,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 30,
         color: "#fff",
-        marginTop: 30,
+        marginTop: 24,
         fontWeight: "700",
     },
 
