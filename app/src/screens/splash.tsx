f9d71401a5c49c690afcc3634206f55032d39174
bhupengit <bhupenjoshi07@gmail.com>
2026-04-28 17:42:55 +0530

    basic update

--- app/src/screens/splash.tsx
+++ app/src/screens/splash.tsx
@@ -158,8 +158,8 @@ const styles = StyleSheet.create({
     },
 
     particle: {
-        width: 6,
-        height: 6,
+        width: 16,
+        height: 16,
         borderRadius: 3,
         backgroundColor: "rgba(255,255,255,0.3)",
     },
