1467e625fccf91d153417064a827f825af4f0053
bhupengit <bhupenjoshi07@gmail.com>
2026-05-11 22:54:08 +0530

    basic update

--- app/src/components/morphButton.tsx
+++ app/src/components/morphButton.tsx
@@ -42,7 +42,7 @@ const RESET_DELAY = 3000; // ms
   
       contentOpacity.value = withTiming(0, { duration: 1500 });
       width.value = withTiming(56, { duration: 2500 });
-      radius.value = withTiming(28, { duration: 250 });
+      radius.value = withTiming(28, { duration: 2500 });
   
       // simulate API call
       setTimeout(() => {
